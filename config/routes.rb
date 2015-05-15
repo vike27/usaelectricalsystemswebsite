d14ff2a816e87a980002de1b436673eedec30dee
Javier Noris <norisjavier@gmail.com>
2015-05-15 15:55:16 -0700

    created location resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :locations
   resources :posts
   resources :services
   # The priority is based upon order of creation: first created -> highest priority.
