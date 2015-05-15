675c3154a23861c4945778d832d143eb6e62a14d
Javier Noris <norisjavier@gmail.com>
2015-05-15 15:59:39 -0700

    added a root home page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :locations
   resources :posts
   resources :services
+
+  root 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
